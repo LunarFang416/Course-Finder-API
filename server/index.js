ee598fa6e44d5b82255bed241f5892acd344affe
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-01-11 20:30:08 -0500

    deployment fixes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const {router} = require("./routes/routes.js");
+const cors = require("cors")
 
 const app = express();
 const PORT = process.env.PORT || 7000;
+app.use(cors())
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Go to /api/v1/ </h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Go to /api/v1/ </h1>");
+// });
 
-app.get("/api/v1", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("<h1>Hello brother</h1>");
 });
 
